67446fca799237e3a7d4a95abd512c2d65274566
Christopher A. Lee <theairhockeyguy@aol.com>
2014-12-04 06:46:58 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ahpa::Application.config.secret_key_base = '26ccb635fd7cd4fce23521674413d3773414393854d3813bf7b0c0150d9990dd133b730e3d2ce1c8c85c774146fd49cb5ed5319cfec2974752aa55ff7dbb4be7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ahpa::Application.config.session_store :cookie_store, key: '_ahpa_session'
